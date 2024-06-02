1ea596a3195cb9894cf2c4f84cb2d45e29302c66
= <theanh24013@gmail.com>
2024-06-03 00:08:28 +0700

    Committing changes to UserContext.js

--- VieLocaLure/client/src/contexts/UserContext.js
+++ VieLocaLure/client/src/contexts/UserContext.js
@@ -6,7 +6,7 @@ export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState({
-        role: 'admin',
+        role: 'user',
         accessToken: null
     });
 
